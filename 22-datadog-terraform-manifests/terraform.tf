41b3e918399d611cee0c21d1f1e7c513d40814ce
VINI1520 <67917489+VINI1520@users.noreply.github.com>
2023-09-16 13:31:42 -0400

    First version

--- 22-datadog-terraform-manifests/terraform.tf
+++ 22-datadog-terraform-manifests/terraform.tf
@@ -22,18 +22,19 @@ terraform {
       version = "~> 0.42.0"
     }
 
-    # Adding Backend as S3 for Remote State Storage
-    backend "s3" {
-      bucket = "terraform-abcd-cluster-api-java"
-      key    = "dev/datadog/terraform.tfstate"
-      region = "us-east-1"
-  
-      # For State Locking
-      dynamodb_table = "dev-terraform-datadog-abcd-api-java-state"    
-    } 
   }
   required_version = ">= 1.1.0"
 }
+
+# Adding Backend as S3 for Remote State Storage
+  backend "s3" {
+    bucket = "terraform-abcd-cluster-api-java"
+    key    = "dev/datadog/terraform.tfstate"
+    region = "us-east-1"
+
+    # For State Locking
+    dynamodb_table = "dev-terraform-datadog-abcd-api-java-state"    
+  }
 
 provider "aws" {
   region = "us-east-1"
