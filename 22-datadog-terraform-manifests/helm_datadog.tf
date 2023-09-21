e144e60ef9cd2b14c0ba1da1c527f1d1b389efb8
VINI1520 <67917489+VINI1520@users.noreply.github.com>
2023-09-21 08:17:27 -0400

    First Version

--- 22-datadog-terraform-manifests/helm_datadog.tf
+++ 22-datadog-terraform-manifests/helm_datadog.tf
@@ -28,6 +28,62 @@ resource "helm_release" "datadog_agent" {
     name  = "datadog.site"
     value = var.datadog_site
   }
+
+  set {
+    name  = "datadog.logs.enabled"
+    value = true
+  }
+
+  set {
+    name  = "datadog.logs.containerCollectAll"
+    value = true
+  }
+
+  set {
+    name  = "datadog.leaderElection"
+    value = true
+  }
+
+  set {
+    name  = "datadog.collectEvents"
+    value = true
+  }
+
+  set {
+    name  = "clusterAgent.enabled"
+    value = true
+  }
+
+  set {
+    name  = "clusterAgent.metricsProvider.enabled"
+    value = true
+  }
+
+  set {
+    name  = "networkMonitoring.enabled"
+    value = true
+  }
+
+  set {
+    name  = "systemProbe.enableTCPQueueLength"
+    value = true
+  }
+
+  set {
+    name  = "systemProbe.enableOOMKill"
+    value = true
+  }
+
+  set {
+    name  = "securityAgent.runtime.enabled"
+    value = true
+  }
+
+  set {
+    name  = "datadog.hostVolumeMountPropagation"
+    value = "HostToContainer"
+  }
+
 }
 
 /*
